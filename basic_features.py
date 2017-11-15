870b9ae712e2d1657ddbc2b94114e8707e0814d8
Mike Suhan <mikesuhan@gmail.com>
2017-11-15 16:36:06 +0400

    Added all subject pronouns

--- basic_features.py
+++ basic_features.py
@@ -16,7 +16,7 @@ Please leave an in-line comment for each tag that
 
 basic_features = {
 
-    # PUNCTUATION
+    ### PUNCTUATION ###
     # Still needed: '    "   -   $   %
 
     ',': ((',', 0),),                       # ,     ,+clp+++
@@ -28,10 +28,21 @@ basic_features = {
     '(': (('(', 0),),                       # (     (++++
     ')': ((')', 0),),                       # )     )+clp+++
 
-    # PRONOUNS
+    ### PRONOUNS ###
     # Note that some pronouns with two separate tags in CLAWS might only have one tag in Biber (e.g. i & we)
 
-    'PPIS1': (('ppla', 0), ('pp1', 1)),     # i     ppla+pp1+++ first person subject pronoun + first person pronoun
-    'PPIS2': (('ppla', 0), ('pp1', 1))      # we    ppla+pp1+++ first person subject pronoun + first person pronoun
+    ## SUBJECT PRONOUNS ##
+    'PPIS1': (('ppla', 0), ('pp1', 1)),     # i         ppla+pp1+++ first person subject pronoun + first person pronoun
+    'PPHS1': (('pp3a', 0), ('pp3', 1)),     # he, she   pp3a+pp3+++ third person subject pronoun + third person personal pronoun
+
+    'PPIS2': (('ppla', 0), ('pp1', 1)),     # we        ppla+pp1+++ first person subject pronoun + first person pronoun
+    'PPHS2': (('pp3a', 0), ('pp3', 1)),     # they   pp3a+pp3+++ third person subject pronoun + third person personal pronoun
+
+    ## YOU AND IT ##
+    # Neither Biber nor CLAWS seems to distinguish between subject and object for you and it
+    'PPH1': (('pp3', 0), ('it', 1)),        # it        pp3+it+++ third person pronoun + third person impersonal pronoun (it)
+    'PPY': (('pp2', 0), ('pp2', 1)),        # you       pp2+pp2+++ second person pronoun + second person pronoun
+
+
 
 }
