2aa223c9c35abff882b34f7584ab3789b6c790d6
Mike Suhan <mikesuhan@gmail.com>
2017-12-14 18:07:25 +0400

    Consolidated `basic_features` and `lexical_match` into `basic_matcher`

--- token_match.py
+++ token_match.py
@@ -7,4 +7,14 @@ token_match = {
         "n't":  (('xnot', 0), ('not', 2), ('0', 4)),        # xnot++not++0
         # Should go through tagging methods for passives and perfect before going through lexical_match()
         # 'done': (('xvbn', 0), ('xvbn', 3))                  # past part. w/ indeterminte function xvbn+++xvbn+
+
+        ### modals ###
+
+        'would': (('md', 0), ('prd', 1)),                   # md+prd+++
+        'shall': (('md', 0), ('prd', 1)),                   # md+prd+++
+        "'ll": (('md', 0), ('prd', 1), ('0', 4)),           # md+prd+++
+        "'d": (('md', 0), ('prd', 1), ('0', 4)),           # md+prd+++
+
+        'cannot': (('md', 0), ('pos', 1)),                  # md+pos+++
+        'could': (('md', 0), ('pos', 1)),                   # md+pos+++
     }
--- token_tag_match.py
+++ token_tag_match.py
@@ -3,9 +3,21 @@ Used to match token-tag pairs with a tag.
 """
 
 token_tag_match = {
-        ('may', 'VM') : (('md', 0), ('pos', 1)),
-        ('might', 'VM') : (('md', 0), ('pos', 1)),
-        ('can', 'VM') : (('md', 0), ('pos', 1)),
-        ('cannot', 'VM') : (('md', 0), ('pos', 1)),
-        ('could', 'VM') : (('md', 0), ('pos', 1)),
+
+        ### Modals ###
+
+        # modals that aren't homographs with other POSs are in token_match
+
+        ## Possibility ##
+        ('may', 'VM') : (('md', 0), ('pos', 1)),                # md+pos+++
+        ('might', 'VM') : (('md', 0), ('pos', 1)),              # md+pos+++
+        ('can', 'VM') : (('md', 0), ('pos', 1)),                # md+pos+++
+
+
+        ## Prediction ##
+
+        ('will', 'VM'): (('md', 0), ('prd', 1)),
+        ('wilt', 'VM'): (('md', 0), ('prd', 1)),
+
+
 }
